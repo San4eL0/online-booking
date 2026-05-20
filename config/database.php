0340f0def73eefb79ede268627ac98db277b4954
San4eL0 <aleksandrsilo207@gmail.com>
2026-05-20 08:27:01 +0700

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,35 @@
+<?php
+// config/database.php
+class Database {
+    private static $instance = null;
+    private $pdo;
+    
+    private function __construct() {
+        $host = 'localhost';
+        $dbname = 'online_booking';
+        $username = 'root';
+        $password = '';
+        
+        $this->pdo = new PDO(
+            "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+            $username,
+            $password,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ]
+        );
+    }
+    
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
